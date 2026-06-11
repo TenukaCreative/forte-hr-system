6fa17363998aa97806ddc6cabe0e9d137f356bf5
Tenuka <tenukaperera1025@gmail.com>
2026-06-11 19:57:29 +0530

    SSL added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,9 +6,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production'
+        ? { require: true, rejectUnauthorized: false }
+        : false,
+    },
   }
 );
 
